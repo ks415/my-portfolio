30ff6314c9176a02d013c4c62b0dcdc46bdc548d
ks <146908390+ks415@users.noreply.github.com>
2025-01-13 14:00:30 +0900

    Worksの更新

--- app/Works.tsx
+++ app/Works.tsx
@@ -8,21 +8,6 @@ const Works = () => {
                 <h2 className="text-4xl font-bold text-center text-black mb-8">Works</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                     <div className="bg-gray-200 p-6 rounded-lg shadow-md">
-                        <Link href="https://todo-react-bice-xi.vercel.app/" target="_blank" rel="noopener noreferrer">
-                            <div className="cursor-pointer">
-                                <div className="w-full h-60 mb-4">
-                                    <img 
-                                        src="/TODO-React.png" 
-                                        alt="Project 1" 
-                                        className="w-full h-full object-cover rounded hover:opacity-80 transition-opacity"
-                                    />
-                                </div>
-                                <h3 className="text-2xl font-bold mb-2 hover:text-gray-600 transition-colors">TODOリスト</h3>
-                            </div>
-                        </Link>
-                        <p className="text-gray-700">Reactを使用したシンプルなTODOリスト</p>
-                    </div>
-                    <div className="bg-gray-200 p-6 rounded-lg shadow-md">
                         <Link href="https://pokemon-app-three-gules.vercel.app/" target="_blank" rel="noopener noreferrer">
                             <div className="cursor-pointer">
                                 <div className="w-full h-60 mb-4">
@@ -51,6 +36,21 @@ const Works = () => {
                             </div>
                         </Link>
                         <p className="text-gray-700">ReactとFirebaseを使用した簡易掲示板</p>
+                    </div>
+                    <div className="bg-gray-200 p-6 rounded-lg shadow-md">
+                        {/* <Link href="https://blog-with-react-and-firebase-six.vercel.app/" target="_blank" rel="noopener noreferrer"> */}
+                            <div className="cursor-pointer">
+                                <div className="w-full h-60 mb-4">
+                                    <img 
+                                        src="/preparing.png" 
+                                        alt="Project 3" 
+                                        className="w-full h-full object-cover rounded hover:opacity-80 transition-opacity"
+                                    />
+                                </div>
+                                <h3 className="text-2xl font-bold mb-2 hover:text-gray-600 transition-colors">準備中</h3>
+                            </div>
+                        {/* </Link> */}
+                        <p className="text-gray-700">準備中</p>
                     </div>
                 </div>
             </div>
